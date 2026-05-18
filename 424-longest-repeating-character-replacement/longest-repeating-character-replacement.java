a0e3460a49a456c73fc381d706a6c4ff82597165
Sheetal Sawkare <sheetals999909@gmail.com>
2026-05-18 16:45:40 +0530

    Time: 116 ms (5.00%) | Memory: 46.4 MB (30.69%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public static int find_max(int[] arr){
+        int max = arr[0];  
+    
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public int characterReplacement(String s, int k) {
+        int l=0, result = Integer.MIN_VALUE;
+        int[] freq = new int[256];
+        
+        for(int h=0; h<s.length(); h++){
+            char ch = s.charAt(h);
+            int index = (int)ch;
+            freq[index] = freq[index]+1;
+            
+            int len = h - l + 1;
+            int max_count = find_max(freq);
+            int diff = len - max_count;
+            
+            while(diff > k){
+                char low_ch = s.charAt(l);
+                int low_index = (int)low_ch;
+                freq[low_index] = freq[low_index]-1;
+                l++;
+                
+                len = h - l + 1;
+                max_count = find_max(freq);
+                diff = len - max_count;
+            }
+            
+            len = h - l + 1;
+            result = Math.max(len, result);
+        }
+        return result;
+    }
+}
